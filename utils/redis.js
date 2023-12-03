4b53ca0176ccd9b78aebabfd94651d7a5ac633a9
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-03 20:47:59 +0100

    create redis server

--- utils/redis.js
+++ utils/redis.js
@@ -2,10 +2,10 @@ import { createClient } from "redis";
 
 class RedisClient {
   constructor() {
-    this.client = createClient();
+    this.client = redis.createClient();
 
-    this.client.on("error", (error) => {
-      console.log(error);
+    this.client.on("error", (err) => {
+      console.error(err);
     });
   }
 
@@ -14,16 +14,39 @@ class RedisClient {
   }
 
   async get(key) {
-    const value = await this.client.get("key");
-    return value;
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
+      });
+    });
   }
 
   async set(key, value, duration) {
-    await this.client.set(key, value, EX, duration);
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, "EX", duration, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
   }
 
   async del(key) {
-    await this.client.del(key);
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, count) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(count > 0);
+        }
+      });
+    });
   }
 }
 
